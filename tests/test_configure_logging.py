b3475a34f39916ae08afa3310a6a6e906d058aa9
henry <henryborchers@yahoo.com>
2021-05-07 11:00:31 -0500

    clear all handlers

--- tests/test_configure_logging.py
+++ tests/test_configure_logging.py
@@ -1,32 +1,32 @@
 import logging
 from unittest.mock import mock_open, patch
+from hathizip import configure_logging
 
 
-def test_configure_logging_returns_a_logger():
-    from hathizip import configure_logging
+def test_configure_logging_returns_a_logger(caplog):
     logger = configure_logging.configure_logger()
-    assert isinstance(logger, logging.Logger)
-    for h in logger.handlers:
-        logger.removeHandler(h)
+    try:
+        assert isinstance(logger, logging.Logger)
+    finally:
+        logger.handlers.clear()
 
 
-def test_configure_logging_with_debug_mode():
-    from hathizip import configure_logging
+def test_configure_logging_with_debug_mode(caplog):
+
     logger = configure_logging.configure_logger(debug_mode=True)
-    assert logger.handlers[0].level == logging.DEBUG
-    for h in logger.handlers:
-        logger.removeHandler(h)
+    try:
+        assert logger.handlers[0].level == logging.DEBUG
+    finally:
+        logger.handlers.clear()
 
 
 def test_configure_logging_with_log_file():
     import os
-    from hathizip import configure_logging
     sample_output_file = os.path.join("some", "output", "logging.log")
-
-    m = mock_open()
-    with patch('hathizip.configure_logging.logging.open', m):
-        logger = configure_logging.configure_logger(log_file=sample_output_file)
-    assert m.called is True
-
-    for h in logger.handlers:
-        logger.removeHandler(h)
+    try:
+        m = mock_open()
+        with patch('hathizip.configure_logging.logging.open', m):
+            logger = configure_logging.configure_logger(log_file=sample_output_file)
+        assert m.called is True
+    finally:
+        logger.handlers.clear()
